dacd2ec19e9395bfcbb696cf4e6740f9f3e4c7e6
Aleksanteri <aleksanteri.heliovaara@siili.com>
2017-09-14 15:06:29 +0300

    moi

--- src/components/Haku.vue
+++ src/components/Haku.vue
@@ -57,7 +57,7 @@ export default {
 
                 })
               }
-              console.log('🚀');
+              console.log('moi');
                 return dataObject;
             });
         });
